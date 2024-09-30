2b60183ee5503eeff19ff026a1637f47d7a74da4
Fantaskink <45458221+Fantaskink@users.noreply.github.com>
2024-09-30 11:36:31 +0200

    Add docker-compose file and db-init files

---
+++ db-init/02-insert-data.sql
@@ -0,0 +1,11 @@
+-- Insert data into parties table
+INSERT INTO parties (party_name) VALUES
+('2016 XXL Freshmen'),
+('Opium'),
+('OVO');
+
+-- Insert data into candidates table
+INSERT INTO candidates (full_name, party_id) VALUES
+('21 Savage', 1),
+('Destroy Lonely', 2),
+('OVO Bartholomeow', 3);
