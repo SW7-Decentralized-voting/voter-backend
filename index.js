d8cb1907157b930bf2260908c28c3e5cb4115c52
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-09-30 13:51:05 +0200

    Merge branch 'dev' of https://github.com/SW7-Decentralized-voting/backend into dev

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import router from './routes/index.js';
 import process from 'process';
+import { getCandidates } from './api/voting.js';
 
-const app = express();
+const app = express()
 const PORT = process.env.PORT || 8888;
 
 app.use('', router);
 
-app.listen(PORT, () => {
-	// eslint-disable-next-line no-console
-  console.log(`Example app listening on port ${PORT}`);
+app.listen(PORT, async () => { // Make the callback function async
+  try {
+    const candidates = await getCandidates(); // Await the promise
+    console.log(`Example app listening on port ${PORT}`);
+    console.log(candidates);
+  } catch (error) {
+    console.error('Error fetching candidates:', error);
+  }
 });
