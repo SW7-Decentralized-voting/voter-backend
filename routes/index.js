4f01e4f87e5d61206c9053bb78bb6409c19712ee
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-10-03 11:27:18 +0200

    feat: added temporary routes for voting (no functionality)

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
+
 import candidateRoutes from './candidateRoutes.js';
 import partyRoutes from './partyRoutes.js';
+import voteRoutes from './voteRoutes.js';
 
 router.get('/', (req, res) => {
 	res.send(router.stack);
@@ -9,5 +11,6 @@ router.get('/', (req, res) => {
 
 router.use('/candidates', candidateRoutes);
 router.use('/parties', partyRoutes);
+router.use('/vote', voteRoutes);
 
 export default router;
