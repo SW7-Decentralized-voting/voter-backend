bd3a47787b82a9cf7ace90565c5ac17b89fb4b62
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-10-15 12:01:02 +0200

    fix: Updated schemas + removed old db setup

--- db-init/connectToDb.js
+++
@@ -1,8 +0,0 @@
-// Use sequelize to connect to postgres database
-import { Sequelize } from 'sequelize';
-const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/candidates', {
-	logging: false,
-	dialect: 'postgres',
-});
-
-export default sequelize;
