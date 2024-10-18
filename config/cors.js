d68fe525eb0eff9504372addcafec66753792b86
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-10-18 11:27:41 +0200

    fix[cors]: updated port to match frontend (cors)

--- config/cors.js
+++ config/cors.js
@@ -2,8 +2,8 @@ import cors from 'cors';
 
 const corsConfig = {
 	origin: [
-		'http://127.0.0.1:5173',
-		'http://localhost:5173',
+		'http://127.0.0.1:3001',
+		'http://localhost:3001',
 	],
 	optionsSuccessStatus: 200,
 	exposedHeaders: ['Content-Range', 'X-Content-Range'],
