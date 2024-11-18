3607697f687a909048fe810e6fa6a7a027c94de4
Jacob Terpe Woods <jacobterpewoods@gmail.com>
2024-11-18 15:05:18 +0100

    fix: deleted deprecated key verification functionality

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ const router = express.Router();
 
 import candidateRoutes from './candidateRoutes.js';
 import partyRoutes from './partyRoutes.js';
-import voteRoutes from './voteRoutes.js';
 import districtRoutes from './districtRoutes.js';
 import constituencyRoutes from './constituencyRoutes.js';
-import keyVerificationRoutes from './keyVerificationRoutes.js';
 import pollingStationRoutes from './pollingStationRoutes.js';
 
 router.get('/', (req, res) => {
@@ -17,8 +15,6 @@ router.use('/candidates', candidateRoutes);
 router.use('/parties', partyRoutes);
 router.use('/nominationDistricts', districtRoutes);
 router.use('/constituencies', constituencyRoutes);
-router.use('/vote', voteRoutes);
-router.use('/key', keyVerificationRoutes);
 router.use('/pollingStations', pollingStationRoutes);
 
 export default router;
